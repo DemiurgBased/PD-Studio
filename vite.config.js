d119f29903a7117e68d5072de157b4512cab9b3e
Demiurg <danzopoi228@mail.ru>
2025-06-28 02:09:05 +0300

    all files prepared for work

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+// import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import injectHTML from 'vite-plugin-html-inject';
+
+export default defineConfig({
+    root: './src',
+    server: {
+        port: 3000,
+        open: true,
+    },
+    build: {
+        outDir: '../build',
+        rollupOptions: {
+            input: {
+                main: './src/index.html'
+            }
+        }
+    },
+    plugins: [
+        injectHTML(),
+        ViteMinifyPlugin(),
+        // ViteImageOptimizer({
+        //     png: {
+        //         // https://sharp.pixelplumbing.com/api-output#png
+        //         quality: 80,
+        //     },
+        //     jpeg: {
+        //         // https://sharp.pixelplumbing.com/api-output#jpeg
+        //         quality: 80,
+        //     },
+        //     jpg: {
+        //         // https://sharp.pixelplumbing.com/api-output#jpeg
+        //         quality: 80,
+        //     },
+        // }),
+    ],
+});
