45c455d6e10d1b2b78b24f26e3a77f4ac14731fc
Demiurg <danzopoi228@mail.ru>
2025-07-04 02:59:46 +0300

    added adaptive hero section

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,7 +1,9 @@
 const burgerBTN = document.querySelector('.burger-btn');
 const headerInner = document.querySelector('.header__inner');
+const body = document.body;
 
 burgerBTN.addEventListener('click', () => {
     burgerBTN.classList.toggle('burger-btn-active');
-    headerInner.classList.toggle('header__inner-active')
+    headerInner.classList.toggle('header__inner-active');
+    body.classList.toggle('no-scroll');
 })
